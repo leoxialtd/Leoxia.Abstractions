aa29feb8cd1c4340bce1263acd3c19d37641a60d
storm <contact@leoxia.com>
2017-06-15 03:45:07 +0700

    Add Diagnostics abstractions. Fix #3. Add Documentation in release. Fix #2. Reduce the .net standard required when possible. Fix #1.

---
+++ src/Leoxia.Abstractions.Diagnostics/IStopWatch.cs
@@ -0,0 +1,104 @@
+﻿#region Copyright (c) 2017 Leoxia Ltd
+
+// MIT License
+// 
+// Copyright (c) 2017 Leoxia Ltd
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#endregion
+
+#region Usings
+
+using System;
+
+#endregion
+
+namespace Leoxia.Abstractions.Diagnostics
+{
+    /// <summary>
+    ///     Provides a set of methods and properties that you can use to accurately measure elapsed time.To browse the
+    ///     .NET Framework source code for this type, see the Reference Source.
+    /// </summary>
+    /// <filterpriority>1</filterpriority>
+    public interface IStopwatch
+    {
+        /// <summary>Gets the total elapsed time measured by the current instance.</summary>
+        /// <returns>
+        ///     A read-only <see cref="T:System.TimeSpan" /> representing the total elapsed time measured by the current
+        ///     instance.
+        /// </returns>
+        /// <filterpriority>1</filterpriority>
+        TimeSpan Elapsed { get; }
+
+        /// <summary>Gets the total elapsed time measured by the current instance, in milliseconds.</summary>
+        /// <returns>A read-only long integer representing the total number of milliseconds measured by the current instance.</returns>
+        /// <filterpriority>1</filterpriority>
+        long ElapsedMilliseconds { get; }
+
+        /// <summary>Gets the total elapsed time measured by the current instance, in timer ticks.</summary>
+        /// <returns>A read-only long integer representing the total number of timer ticks measured by the current instance.</returns>
+        /// <filterpriority>1</filterpriority>
+        long ElapsedTicks { get; }
+
+        /// <summary>Gets the frequency of the timer as the number of ticks per second. This field is read-only.</summary>
+        /// <filterpriority>1</filterpriority>
+        long Frequency { get; }
+
+        /// <summary>Indicates whether the timer is based on a high-resolution performance counter. This field is read-only.</summary>
+        /// <filterpriority>1</filterpriority>
+        bool IsHighResolution { get; }
+
+        /// <summary>Gets a value indicating whether the <see cref="T:System.Diagnostics.Stopwatch" /> timer is running.</summary>
+        /// <returns>
+        ///     true if the <see cref="T:System.Diagnostics.Stopwatch" /> instance is currently running and measuring elapsed
+        ///     time for an interval; otherwise, false.
+        /// </returns>
+        /// <filterpriority>1</filterpriority>
+        bool IsRunning { get; }
+
+        /// <summary>Gets the current number of ticks in the timer mechanism.</summary>
+        /// <returns>A long integer representing the tick counter value of the underlying timer mechanism.</returns>
+        /// <filterpriority>1</filterpriority>
+        long GetTimestamp();
+
+        /// <summary>Stops time interval measurement and resets the elapsed time to zero.</summary>
+        /// <filterpriority>1</filterpriority>
+        void Reset();
+
+        /// <summary>Stops time interval measurement, resets the elapsed time to zero, and starts measuring elapsed time.</summary>
+        void Restart();
+
+        /// <summary>Starts, or resumes, measuring elapsed time for an interval.</summary>
+        /// <filterpriority>1</filterpriority>
+        void Start();
+
+        /// <summary>
+        ///     Initializes a new <see cref="T:System.Diagnostics.Stopwatch" /> instance, sets the elapsed time property to
+        ///     zero, and starts measuring elapsed time.
+        /// </summary>
+        /// <returns>A <see cref="T:System.Diagnostics.Stopwatch" /> that has just begun measuring elapsed time.</returns>
+        /// <filterpriority>1</filterpriority>
+        IStopwatch StartNew();
+
+        /// <summary>Stops measuring elapsed time for an interval.</summary>
+        /// <filterpriority>1</filterpriority>
+        void Stop();
+    }
+}
---
+++ src/Leoxia.Implementations.Diagnostics/StopwatchAdapter.cs
@@ -0,0 +1,136 @@
+﻿#region Copyright (c) 2017 Leoxia Ltd
+
+// MIT License
+// 
+// Copyright (c) 2017 Leoxia Ltd
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#endregion
+
+#region Usings
+
+using System;
+using System.Diagnostics;
+using Leoxia.Abstractions.Diagnostics;
+
+#endregion
+
+namespace Leoxia.Implementations.Diagnostics
+{
+    /// <summary>
+    ///     Implementation of <see cref="IStopwatch" />
+    /// </summary>
+    public class StopwatchAdapter : IStopwatch
+    {
+        private readonly Stopwatch _stopwatchImplementation;
+
+        /// <summary>Initializes a new instance of the <see cref="T:System.Object" /> class.</summary>
+        public StopwatchAdapter()
+        {
+            _stopwatchImplementation = new Stopwatch();
+        }
+
+        /// <summary>Initializes a new instance of the <see cref="T:System.Object" /> class.</summary>
+        public StopwatchAdapter(Stopwatch stopwatchImplementation)
+        {
+            _stopwatchImplementation = stopwatchImplementation;
+        }
+
+        /// <summary>Gets the frequency of the timer as the number of ticks per second. This field is read-only.</summary>
+        /// <filterpriority>1</filterpriority>
+        public long Frequency => Stopwatch.Frequency;
+
+        /// <summary>Indicates whether the timer is based on a high-resolution performance counter. This field is read-only.</summary>
+        /// <filterpriority>1</filterpriority>
+        public bool IsHighResolution => Stopwatch.IsHighResolution;
+
+        /// <summary>Gets the total elapsed time measured by the current instance.</summary>
+        /// <returns>
+        ///     A read-only <see cref="T:System.TimeSpan" /> representing the total elapsed time measured by the current
+        ///     instance.
+        /// </returns>
+        /// <filterpriority>1</filterpriority>
+        public TimeSpan Elapsed => _stopwatchImplementation.Elapsed;
+
+        /// <summary>Gets the total elapsed time measured by the current instance, in milliseconds.</summary>
+        /// <returns>A read-only long integer representing the total number of milliseconds measured by the current instance.</returns>
+        /// <filterpriority>1</filterpriority>
+        public long ElapsedMilliseconds => _stopwatchImplementation.ElapsedMilliseconds;
+
+        /// <summary>Gets the total elapsed time measured by the current instance, in timer ticks.</summary>
+        /// <returns>A read-only long integer representing the total number of timer ticks measured by the current instance.</returns>
+        /// <filterpriority>1</filterpriority>
+        public long ElapsedTicks => _stopwatchImplementation.ElapsedTicks;
+
+        /// <summary>Gets a value indicating whether the <see cref="T:System.Diagnostics.Stopwatch" /> timer is running.</summary>
+        /// <returns>
+        ///     true if the <see cref="T:System.Diagnostics.Stopwatch" /> instance is currently running and measuring elapsed
+        ///     time for an interval; otherwise, false.
+        /// </returns>
+        /// <filterpriority>1</filterpriority>
+        public bool IsRunning => _stopwatchImplementation.IsRunning;
+
+        /// <summary>Gets the current number of ticks in the timer mechanism.</summary>
+        /// <returns>A long integer representing the tick counter value of the underlying timer mechanism.</returns>
+        /// <filterpriority>1</filterpriority>
+        public long GetTimestamp()
+        {
+            return Stopwatch.GetTimestamp();
+        }
+
+        /// <summary>Stops time interval measurement and resets the elapsed time to zero.</summary>
+        /// <filterpriority>1</filterpriority>
+        public void Reset()
+        {
+            _stopwatchImplementation.Reset();
+        }
+
+        /// <summary>Stops time interval measurement, resets the elapsed time to zero, and starts measuring elapsed time.</summary>
+        public void Restart()
+        {
+            _stopwatchImplementation.Restart();
+        }
+
+        /// <summary>Starts, or resumes, measuring elapsed time for an interval.</summary>
+        /// <filterpriority>1</filterpriority>
+        public void Start()
+        {
+            _stopwatchImplementation.Start();
+        }
+
+        /// <summary>
+        ///     Initializes a new <see cref="T:System.Diagnostics.Stopwatch" /> instance, sets the elapsed time property to
+        ///     zero, and starts measuring elapsed time.
+        /// </summary>
+        /// <returns>A <see cref="T:System.Diagnostics.Stopwatch" /> that has just begun measuring elapsed time.</returns>
+        /// <filterpriority>1</filterpriority>
+        public IStopwatch StartNew()
+        {
+            return new StopwatchAdapter(Stopwatch.StartNew());
+        }
+
+        /// <summary>Stops measuring elapsed time for an interval.</summary>
+        /// <filterpriority>1</filterpriority>
+        public void Stop()
+        {
+            _stopwatchImplementation.Stop();
+        }
+    }
+}
